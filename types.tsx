cfda827776a8c323c069b9a35da4ecf32cf6e415
Deivis Dervinis <ddrdeivis@gmail.com>
2021-05-03 02:00:56 -0400

    Added initial files

---
+++ types.tsx
@@ -0,0 +1,27 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  SearchTab: undefined;
+  TabThree: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type SearchTabParamList = {
+  SearchTabScreen: undefined;
+};
+
+export type TabThreeParamList = {
+  TabThreeScreen: undefined;
+};
